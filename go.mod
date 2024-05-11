1b7a827cd5ad4d3f2f621bf448d360d782d71749
Younes Outerbah <younesouterbah1@gmail.com>
2024-05-11 16:43:18 +0100

    clear the project for ds code

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/YounesOuterbah/goDS.git
 
 go 1.22.2
-
-require github.com/boombuler/barcode v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"image/png"
-	"net/http"
-	"text/template"
-
-	"github.com/boombuler/barcode"
-	"github.com/boombuler/barcode/qr"
-)
-
-type Page struct {
-	Title string
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/generator/", viewCodeHandler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	p1 := Page{Title: "QR-Code-Generator"}
-	t1, _ := template.ParseFiles("genrator.html")
-	t1.Execute(w, p1)
-}
-
-func viewCodeHandler(w http.ResponseWriter, r *http.Request) {
-	dataString := r.FormValue("dataString")
-	qrCode, _ := qr.Encode(dataString, qr.L, qr.Auto)
-	qrCode, _ = barcode.Scale(qrCode, 512, 512)
-	png.Encode(w, qrCode)
+	fmt.Print("DS WITH GO")
 }
